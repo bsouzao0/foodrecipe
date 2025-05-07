639dc43cba8703193442d8e913307589d6b7f3ed
Brenda Sayonara <149035540+bsouzao0@users.noreply.github.com>
2025-05-06 21:47:24 -0400

    first-commit

---
+++ backend/models/recipe.js
@@ -0,0 +1,29 @@
+const mongoose=require('mongoose')
+
+const recipeSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    ingredients:{
+        type:Array,
+        required:true
+    },
+    instructions:{
+        type:String,
+        required:true
+    },
+    time:{
+        type:String,
+    },
+    coverImage:{
+        type:String,
+    },
+    createBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    }
+
+},{timestamps:true})
+
+module.exports=mongoose.model('Recipe',recipeSchema)
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <p>Build following the tutorial from <a href="https://www.youtube.com/@imaginaryengineering5566">Imaginary Engineering</a></p>
+        </div>
+    )
+}
