bc8e8544fd867e6500ca7df5fb30999f68561507
Brenda Sayonara <149035540+bsouzao0@users.noreply.github.com>
2025-05-08 22:33:59 -0400

    b

--- backend/controller/recipe.js
+++ backend/controller/recipe.js
@@ -1,68 +1,90 @@
-const Recipes=require('../models/recipe')
-const multer=require('multer')
+const Recipes = require('../models/recipe');
+const multer = require('multer');
+const path = require('path');
+
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/images')
-    },
-    filename: function (req, file, cb) {
-      const filename = Date.now() + '-' + file.filename
-      cb(null, file.filename)
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, './public/images'); 
+  },
+  filename: function (req, file, cb) {
+    const fileExtension = path.extname(file.originalname); // Get file extension
+    const filename = Date.now() + '-' + file.fieldname + fileExtension; // Generate a unique filename
+    cb(null, filename); // Save the file with the generated filename
+  }
+});
 
-const getRecipes=async(req,res)=>{
-    const recipes=await Recipes.find()
-    return res.json(recipes)
-}
 
-const getRecipe=async(req,res)=>{
-    const recipe=await Recipes.findById(req.params.id)
-    res.json(recipe)
-}
+const upload = multer({ storage: storage });
 
-const addRecipe=async(req,res)=>{
-    console.log(req.user)
-    const {title,ingredients,instructions,time}=req.body
+const getRecipes = async (req, res) => {
+  try {
+    const recipes = await Recipes.find();
+    return res.json(recipes);
+  } catch (err) {
+    return res.status(500).json({ message: "Error fetching recipes", error: err });
+  }
+};
 
-    if(!title || !ingredients || !instructions) 
-    {
-        res.json({message:"Required fields can't be empty"})
-    }
+const getRecipe = async (req, res) => {
+  try {
+    const recipe = await Recipes.findById(req.params.id);
+    if (!recipe) return res.status(404).json({ message: "Recipe not found" });
+    res.json(recipe);
+  } catch (err) {
+    return res.status(500).json({ message: "Error fetching recipe", error: err });
+  }
+};
 
-    const newRecipe=await Recipes.create({
-        title,ingredients,instructions,time,coverImage:req.file.filename,
-        createBy:req.user.id
-    })
-    return res.json(newRecipe)
-}
+const addRecipe = async (req, res) => {
+  const { title, ingredients, instructions, time } = req.body;
 
-const editRecipe=async(req,res)=>{
-    const {title,ingredients,instructions,time}=req.body
-    let recipe=await Recipes.findById(req.params.id)
+  if (!title || !ingredients || !instructions) {
+    return res.status(400).json({ message: "Required fields can't be empty" });
+  }
 
-    try{
-        if(recipe){
-            let coverImage=req.file?.filename ?req.file?.filename : recipe.coverImage
-            await Recipes.findByIdAndUpdate(req.params.id,{...req.body,coverImage},{new:true})
-            res.json({title,ingredients,instructions,time})
-        }
-    }
-    catch(err){
-        return res.status(404).json({message:"error"})
-    }
-}
-const deleteRecipe=async(req,res)=>{
-    try{
-        await Recipes.deleteOne({_id:req.params.id})
-        res.json({status:"ok"})
-    }
-    catch(err){
-        return res.status(400).json({message:"error"})
+  try {
+    const newRecipe = await Recipes.create({
+      title,
+      ingredients,
+      instructions,
+      time,
+      coverImage: req.file ? req.file.filename : undefined, 
+      createdBy: req.user.id, 
+    });
+    return res.json(newRecipe);
+  } catch (err) {
+    return res.status(500).json({ message: "Error creating recipe", error: err });
+  }
+};
 
-    }
-}
+const editRecipe = async (req, res) => {
+  const { title, ingredients, instructions, time } = req.body;
+  try {
+    let recipe = await Recipes.findById(req.params.id);
+    if (!recipe) return res.status(404).json({ message: "Recipe not found" });
 
-module.exports={getRecipes,getRecipe,addRecipe,editRecipe,deleteRecipe,upload}
+    const coverImage = req.file ? req.file.filename : recipe.coverImage; // If file uploaded, update cover image
+
+    const updatedRecipe = await Recipes.findByIdAndUpdate(
+      req.params.id,
+      { title, ingredients, instructions, time, coverImage },
+      { new: true }
+    );
+
+    res.json(updatedRecipe);
+  } catch (err) {
+    return res.status(500).json({ message: "Error updating recipe", error: err });
+  }
+};
+
+const deleteRecipe = async (req, res) => {
+  try {
+    await Recipes.deleteOne({ _id: req.params.id });
+    res.json({ status: "ok" });
+  } catch (err) {
+    return res.status(400).json({ message: "Error deleting recipe", error: err });
+  }
+};
+
+module.exports = { getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe, upload };
