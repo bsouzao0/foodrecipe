49848183736a5cd1aa2b37d82be4db53c754d227
Brenda Sayonara <149035540+bsouzao0@users.noreply.github.com>
2025-05-08 21:36:04 -0400

    mo

--- backend/config/connectionDb.js
+++ backend/config/connectionDb.js
@@ -1,8 +1,17 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI)
-    .then(()=>console.log('connected...'))
-}
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables.");
+    }
 
-module.exports=connectDB
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); 
+  }
+};
+
+module.exports = connectDB;
