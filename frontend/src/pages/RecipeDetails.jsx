aff976b928ef26feaab6bcc5dbd9ad39e2ed9958
Brenda Sayonara <149035540+bsouzao0@users.noreply.github.com>
2025-05-09 02:19:52 -0400

    g

--- frontend/src/pages/RecipeDetails.jsx
+++ frontend/src/pages/RecipeDetails.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import profileImg from '../assets/profile.png'
-import { useLoaderData } from 'react-router-dom'
+import React from 'react';
+import profileImg from '../assets/profile.png';
+import { useLoaderData } from 'react-router-dom';
 
 export default function RecipeDetails() {
-    const recipe=useLoaderData()
-    console.log(recipe)
-  return (
-   <>
-    <div className='outer-container'>
-        <div className='profile'>
-            <img src={profileImg} width="50px" height="50px"></img>
-            <h5>{recipe.email}</h5>
+    const recipe = useLoaderData();
+    if (!recipe) return <p>Loading...</p>;
+
+    return (
+        <div className='outer-container'>
+            <div className='profile'>
+                <img src={profileImg} width="50px" height="50px" alt="Profile" />
+                <h5>{recipe.email}</h5>
+            </div>
+            <h3 className='title'>{recipe.title}</h3>
+            <img src={`https://foodrecipe-8brr.onrender.com/${recipe.coverImage}`} width="220px" height="200px" alt="Recipe Cover" />
+            <div className='recipe-details'>
+                <div className='ingredients'>
+                    <h4>Ingredients</h4>
+                    <ul>{recipe.ingredients.map((item, index) => (<li key={index}>{item}</li>))}</ul>
+                </div>
+                <div className='instructions'>
+                    <h4>Instructions</h4>
+                    <span>{recipe.instructions}</span>
+                </div>
+            </div>
         </div>
-        <h3 className='title'>{recipe.title}</h3>
-        <img src={`https://foodrecipe-8brr.onrender.com/${recipe.coverImage}`} width="220px" height="200px"></img>
-        <div className='recipe-details'>
-            <div className='ingredients'><h4>Ingredients</h4><ul>{recipe.ingredients.map(item=>(<li>{item}</li>))}</ul></div>
-            <div className='instructions'><h4>Instructions</h4><span>{recipe.instructions}</span></div>
-        </div>
-    </div>
-   </>
-  )
+    );
 }
